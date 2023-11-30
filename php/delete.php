5d4939b0121495980db8918445366e506329f3bd
Andreibatman <oficialandreicroitoriu@yahoo.com>
2023-12-01 00:04:03 +0200

    Property count working

--- php/delete.php
+++ php/delete.php
@@ -1,39 +1,58 @@
 <?php
 // Process delete operation after confirmation
-if(isset($_POST["id"]) && !empty($_POST["id"])){
+if (isset($_POST["id"]) && !empty($_POST["id"])) {
     // Include config file
     require_once "config.php";
-    
+
+    // Start a session to get the user ID if it's not already started
+    session_start();
+
     // Prepare a delete statement
-    $sql = "DELETE FROM posts WHERE id = ?";
-    
-    if($stmt = mysqli_prepare($link, $sql)){
+    $sql_delete_post = "DELETE FROM posts WHERE id = ?";
+    $sql_update_user_property = "UPDATE users SET property = property - 1 WHERE id = ?";
+
+    if ($stmt_delete_post = mysqli_prepare($link, $sql_delete_post)) {
         // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "i", $param_id);
-        
+        mysqli_stmt_bind_param($stmt_delete_post, "i", $param_id_delete);
+
         // Set parameters
-        $param_id = trim($_POST["id"]);
-        
+        $param_id_delete = trim($_POST["id"]);
+
         // Attempt to execute the prepared statement
-        if(mysqli_stmt_execute($stmt)){
-            // Posts deleted successfully. Redirect to landing page
+        if (mysqli_stmt_execute($stmt_delete_post)) {
+            // If the post is deleted successfully, update the user's property value
+            if ($stmt_update_user_property = mysqli_prepare($link, $sql_update_user_property)) {
+                // Bind variables to the prepared statement as parameters
+                mysqli_stmt_bind_param($stmt_update_user_property, "i", $param_user_id);
+
+                // Set parameters
+                $param_user_id = $_SESSION["id"]; // Assuming you store the user ID in a session
+
+                // Attempt to execute the prepared statement
+                mysqli_stmt_execute($stmt_update_user_property);
+
+                // Close the update statement
+                mysqli_stmt_close($stmt_update_user_property);
+            }
+
+            // Redirect to the landing page
             header("location: dashboard.php");
             exit();
-        } else{
+        } else {
             echo "Oops! Something went wrong. Please try again later.";
         }
     }
-     
+
     // Close statement
-    mysqli_stmt_close($stmt);
-    
+    mysqli_stmt_close($stmt_delete_post);
+
     // Close connection
     mysqli_close($link);
-} else{
+} else {
     // Check existence of id parameter
-    if(empty(trim($_GET["id"]))){
+    if (empty(trim($_GET["id"]))) {
         // URL doesn't contain id parameter. Redirect to error page
-        header("location: error.php");
+        header("location: dashboard.php");
         exit();
     }
 }
